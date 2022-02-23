02d4177acc111d362d7b71cb2209a077f931c8f3
Yuliia Hnatyshyn <julia.gnatyshyn@gmail.com>
2022-02-23 23:44:27 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,9 +8,9 @@ gallery.innerHTML = markUp;
 function galleryMarkUp(items) {
   return items
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
+      return `<li><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
+</a></li>`;
     })
     .join('');
 }
